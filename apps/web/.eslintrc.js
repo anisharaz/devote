fce80c9daddf416fbb35103684c99436c284861d
Anish Araz <anisharaz919@gmail.com>
2024-06-25 10:34:13 +0530

    FEAT: bootstrap base code

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@repo/eslint-config/next.cjs"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
+  ignorePatterns: ["tailwind.config.cjs", "next.config.mjs", "postcss.config.cjs"],
 };
