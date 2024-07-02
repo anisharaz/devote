92130308f6b75f7585d3f62411a521c7a9a8edda
Anish Araz <anisharaz919@gmail.com>
2024-07-03 00:33:03 +0530

    FEAT: admin can view number of votes of candidates

---
+++ apps/identity/app/registrar/viewvotes/[votinglevelid]/loading.tsx
@@ -0,0 +1,9 @@
+function ViewVotingLoading() {
+  return (
+    <div className="main-body flex justify-center items-center">
+      <div className="text-4xl text-white">Loading....</div>
+    </div>
+  );
+}
+
+export default ViewVotingLoading;
--- apps/identity/app/registrar/viewvotes/[votinglevelid]/page.tsx
+++ apps/identity/app/registrar/viewvotes/[votinglevelid]/page.tsx
@@ -1,7 +1,85 @@
-function NumberOfVotes({ params }: { params: { votinglevelid: string } }) {
+import { prisma } from "@repo/prismadb";
+import axios from "axios";
+import Image from "next/image";
+
+async function NumberOfVotes({
+  params,
+}: {
+  params: { votinglevelid: string };
+}) {
+  const candidates = await prisma.candidates.findMany({
+    where: {
+      levelId: params.votinglevelid,
+    },
+  });
   return (
     <div className="main-body flex flex-col justify-center items-center">
-      <div>{params.votinglevelid}</div>
+      <div className="flex flex-wrap gap-6">
+        {candidates.map(async (candidate) => {
+          const tokenaccount = await axios({
+            url: process.env.RPC_URL,
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            data: [
+              {
+                jsonrpc: "2.0",
+                id: 1,
+                method: "getTokenAccountsByOwner",
+                params: [
+                  candidate.WalletAddress,
+                  {
+                    mint: "J7PEVpHoy8ZM4kaXtHFwAHb8mwQWKZyM8UHHZUrFJ1u9",
+                  },
+                  {
+                    encoding: "jsonParsed",
+                  },
+                ],
+              },
+            ],
+          });
+          const balance = await axios({
+            url: process.env.RPC_URL,
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            data: [
+              {
+                jsonrpc: "2.0",
+                id: 1,
+                method: "getTokenAccountBalance",
+                params: [tokenaccount.data[0].result.value[0]?.pubkey],
+              },
+            ],
+          });
+
+          return (
+            <div key={candidate.id}>
+              <div className="bg-white border w-80 h-auto border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <Image
+                  className="rounded-t-lg object-cover w-full h-48"
+                  src={candidate.image}
+                  alt="CandiateImage"
+                  width={800}
+                  height={600}
+                  priority
+                />
+                <div className="p-5">
+                  <p className="mb-2 text-2xl font-bold tracking-tight text-black">
+                    {candidate.name}
+                  </p>
+                  <p className="mb-3 font-normal text-black dark:text-gray-600">
+                    {candidate.statement}
+                  </p>
+                  <div className="text-4xl text-blue-600">
+                    {balance.data[0].result?.value?.amount
+                      ? balance.data[0].result?.value?.amount / 1000000000
+                      : 0}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
